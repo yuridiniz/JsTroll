3935f881e0466091da66aab8faeb677f4b20b103
Yuri Araújo <yschmitzz@hotmail.com>
2014-08-01 14:25:00 -0300

    Remoção do load

--- Analytic/t.js
+++ Analytic/t.js
@@ -1,22 +1,4 @@
-﻿
-function loadjscssfile(filename, filetype){
- if (filetype=="js"){ //if filename is a external JavaScript file
-  var fileref=document.createElement('script')
-  fileref.setAttribute("type","text/javascript")
-  fileref.setAttribute("src", filename)
- }
- else if (filetype=="css"){ //if filename is an external CSS file
-  var fileref=document.createElement("link")
-  fileref.setAttribute("rel", "stylesheet")
-  fileref.setAttribute("type", "text/css")
-  fileref.setAttribute("href", filename)
- }
- if (typeof fileref!="undefined")
-  document.getElementsByTagName("head")[0].appendChild(fileref)
-}
-
-
-/*
+﻿/*
 * Variável que verifica se toda a trolagem esta ativa ou não
 * TrollDelay: Tempo em minutos para habilitar a proxima trolagem
 */
